d16395c9287bc04d18b2c9ac52d0ab9b1e365537
Student101 <student101@PSAdmins-Air-3.home>
2020-04-17 13:12:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component} from 'react';
+import './App.css'
+class App extends Component {
+  constructor(){
+    super()
+    this.state= {
+      items:null,
+      isLoaded: false
+    }
+  }
+  componentDidMount() {
+    fetch('https://picsum.photos/v2/list?page=2&limit=20')
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+      this.setState({
+        isLoaded: true,
+        items: result
+      });
+    });
+    
+      
+  }
+  render() {
+    const {  isLoaded, items } = this.state;
+     if (!isLoaded) {
+      return <div className="loading">Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.author} 
+              <img src={item.download_url} />
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
